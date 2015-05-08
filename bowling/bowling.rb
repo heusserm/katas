83acd0b3ce2a6fc7c981d53e37225dcf9865952c
heusserm <matt.heusser@gmail.com>
2015-05-08 13:38:26 -0400

    made the game a 2d array

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,9 +1,15 @@
 
 class Game
+  #frames is an array of arrays.
   attr :frames;
 
-  def initialize(pinsRolled)
-    @frames = pinsRolled
+  def roll(pin1, pin2)
+    frame = [pin1, pin2]
+    frames.push(frame);
+  end
+
+  def initialize()
+    @frames = Array.new();
   end
 
   def result
@@ -13,16 +19,20 @@ class Game
 
   def calcscore
     total = 0
-    frames.each do |roll|
-      total+=roll
+    frames.each do |frame|
+      frame.each do |roll|
+        total+=roll
+      end
     end
     return total
   end
 
 end
 
-pins = Array.new(10) { |e| e = 1 };
-puts "#{pins}";
-playing = Game.new(pins);
+playing = Game.new();
+playing.roll(1,1);
+playing.roll(1,1);
+playing.roll(1,1);
+playing.roll(1,1);
 score = playing.result();
 puts "Total score is: " + score.to_s();
