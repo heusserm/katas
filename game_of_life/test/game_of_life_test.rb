fda00902f9340a99ffb2ff66b75ee8cc7ea646bf
heusserm <matt.heusser@gmail.com>
2017-06-14 22:18:18 -0400

    test a 2x2 grid complete for all null values

--- game_of_life/test/game_of_life_test.rb
+++ game_of_life/test/game_of_life_test.rb
@@ -7,7 +7,7 @@ class TestGame_of_life < MiniTest::Test
     playing = Game_of_life.new()
     assert(playing!=nil,"Created object")
     assert(playing.grid.kind_of?(Array),"Loadstring creates a array");
-    assert(playing.grid.length==0,"And the array is empty");
+    assert_equal(0,playing.grid.length,"And the array is empty");
   end
 
    def test_simple_read
@@ -18,15 +18,15 @@ class TestGame_of_life < MiniTest::Test
 EOF
      playing = Game_of_life.new()
      playing.loadstring(twoxtwoblank);
-     assert(playing.grid.length()==2,"grid is an array with two elements");
-     playing.grid.each { | y |
-        y.each { | x | 
-          numX=numX+1;
-        }
-       }
+     #First Dimensional array is 2 
+     assert_equal(2, playing.grid.length,"grid is an array with two elements");
+     
+     #each sub-dimensional array is 2
      playing.grid.each { | y |
          numrow = 0;
-         assert(y.length()==2,"Length of Y at " + numrow.to_s() + " is two");
+         assert_equal(2, y.length(),"Length of Y at " + numrow.to_s() + " is two");
+         assert_equal(".",y[0],"First element is a dot");
+         assert_equal(".",y[1],"second element is a dot");
          numrow=numrow+1;
      }
    threexthreeoneinthemiddle=<<EOF
