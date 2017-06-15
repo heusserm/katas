47f9d3a5f370b18ea53b4cd593aa87591c4349d9
heusserm <matt.heusser@gmail.com>
2017-06-15 08:47:35 -0400

    Cleaned up some comments in a test and added a kind_of test for the view's game

--- game_of_life/test/view_test.rb
+++ game_of_life/test/view_test.rb
@@ -9,9 +9,9 @@ class TestGame_of_life_view < MiniTest::Test
        def test_create_view
          playing = Game_of_life.new();
          view = Game_Of_Life_View.new(playing);
-         assert(view.kind_of?(Game_Of_Life_View),"Loadstring creates a array");
-         
-         assert(view.game!=nil,"Game created on constructor");
+         assert(view.kind_of?(Game_Of_Life_View),"View is a game of life view");         
+         assert(view.game!=nil,"Game is passed into constructor");
+         assert(view.game.kind_of?(Game_of_life),"...and it is a game");
        end
 
 end
