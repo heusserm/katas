f681315281cd171d8008aa3943b263c9a6881536
heusserm <matt.heusser@gmail.com>
2017-06-15 09:33:25 -0400

    Display zeroday to terminal from disk

--- game_of_life/game_of_life.rb
+++ game_of_life/game_of_life.rb
@@ -1,5 +1,16 @@
 #game_of_life.rb
 
 require_relative 'lib/game_of_life_lib.rb'
-game_of_life=Game_of_life.new()
+require_relative 'lib/game_of_life_view.rb'
 
+puts "\n\n-------------------------------------------------------";
+puts "Conway's Game Of Life Spike\nBy Matthew Heusser Matt@xndev.com\nToday this just reads in a day0 game of life from disk\n";
+puts "-------------------------------------------------------\n\n"
+
+game = Game_of_life.new()
+game.loadfile('./sample_starting_files/5x5.txt');
+
+view = Game_Of_Life_View.new(game);
+view.display();
+
+puts "\n\n\n";
