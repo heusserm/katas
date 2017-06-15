aabba8e7a7a376bf0cc393429f13df4c0f53a2a5
heusserm <matt.heusser@gmail.com>
2017-06-14 21:53:56 -0400

    first start of setup of grid

--- game_of_life/lib/game_of_life_lib.rb
+++ game_of_life/lib/game_of_life_lib.rb
@@ -2,8 +2,28 @@
 
 class Game_of_life
 
-  def initialize()
+  attr_accessor :grid;
+      
+  def loadstring(gridasstring)
+    array1 = gridasstring.split(/\n/);
+    indexA = 0;
+    indexB = 0;
+    array1.each {|line|
+      indexB = 0; 
+      array2=line.split(//);
+        array2.each {|char|
+          @grid.push([indexB,char]);
+        }
+      indexA = indexA+1;
+    }
+  end
 
+  def loadfile(filemame)
+
+  end
+
+  def initialize()
+    @grid = Array.new(){Array.new()};
   end
 end
 
--- game_of_life/test/game_of_life_test.rb
+++ game_of_life/test/game_of_life_test.rb
@@ -6,6 +6,27 @@ class TestGame_of_life < MiniTest::Test
   def test_init 
     playing = Game_of_life.new()
     assert(playing!=nil,"Created object")
+    assert(playing.grid.kind_of?(Array),"Loadstring creates a array");
+    assert(playing.grid.length==0,"And the array is empty");
   end
+
+   def test_simple_read
+
+  twoxtwoblank=<<EOF
+..
+..
+EOF
+     playing = Game_of_life.new()
+     playing.loadstring(twoxtwoblank);
+     assert(playing.grid.kind_of?(Array),"Loadstring creates a array"); 
+
+   threexthreeoneinthemiddle=<<EOF
+   ...
+   .*.
+   ...
+EOF
+
+  end
+     
 
 end
