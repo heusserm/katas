6ed39596577177ea5807ed053169e09a46d04802
heusserm <matt.heusser@gmail.com>
2017-06-18 06:57:29 -0400

    basic game to console. Still needs to repaint itself instead of puts

--- game_of_life/game_of_life.rb
+++ game_of_life/game_of_life.rb
@@ -4,20 +4,28 @@ require_relative 'lib/game_of_life_lib.rb'
 require_relative 'lib/game_of_life_view.rb'
 
 puts "\n\n-------------------------------------------------------";
+puts "game_of_lie.rb (numrums) (filename)";
+puts " ";
+puts "numruns - Number of generations to run";
+puts "filename - Path to file to load";
+puts " ";
 puts "Conway's Game Of Life Spike\nBy Matthew Heusser Matt@xndev.com\nToday this just reads in a day0 game of life from disk\n";
 puts "-------------------------------------------------------\n\n"
 
 game = Game_of_life.new()
-game.loadfile('./sample_starting_files/10x10.txt');
 
+numruns = ARGV[0]
+filename = ARGV[1]
+
+game.loadfile(filename);
 
 view = Game_Of_Life_View.new(game);
 
-for i in 0..4
-
+for i in 0..(numruns.to_i()-1)
   view.display();
   puts "\n\n\n";
-  view =  Game_Of_Life_View.new(game.nextgen);
+  game = game.nextgen();
+  view = Game_Of_Life_View.new(game);
   sleep(1);
 end
 
