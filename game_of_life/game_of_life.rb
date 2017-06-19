86b64c1b9852c95dc35d74c34594a785ec5ddb4e
heusserm <matt.heusser@gmail.com>
2017-06-18 20:04:04 -0400

    Finalizing main routine. Need to put this into an object?

--- game_of_life/game_of_life.rb
+++ game_of_life/game_of_life.rb
@@ -2,31 +2,40 @@
 
 require_relative 'lib/game_of_life_lib.rb'
 require_relative 'lib/game_of_life_view.rb'
+require_relative 'lib/cursor_wrap.rb'
 
 puts "\n\n-------------------------------------------------------";
-puts "game_of_lie.rb (numrums) (filename)";
+puts "game_of_life.rb (numrums) (filename)";
 puts " ";
 puts "numruns - Number of generations to run";
 puts "filename - Path to file to load";
 puts " ";
-puts "Conway's Game Of Life Spike\nBy Matthew Heusser Matt@xndev.com\nToday this just reads in a day0 game of life from disk\n";
+puts "Conway's Game Of Life ASCII\nBy Matthew Heusser Matt@xndev.com\nToday this just reads in a day0 game of life from disk\n";
 puts "-------------------------------------------------------\n\n"
 
-game = Game_of_life.new()
 
 numruns = ARGV[0]
 filename = ARGV[1]
 
+#Initialize Objects
+game = Game_of_life.new()
 game.loadfile(filename);
-
 view = Game_Of_Life_View.new(game);
+cursor = Cursor_Wrap.new();
 
+#Would you like to play a game?
 for i in 0..(numruns.to_i()-1)
+  puts " Round: " + (i+1).to_s() + "\n\n";
   view.display();
   puts "\n\n\n";
-  game = game.nextgen();
-  view = Game_Of_Life_View.new(game);
-  sleep(1);
+  if (i<numruns.to_i()-1)
+    game = game.nextgen();
+    view = Game_Of_Life_View.new(game);
+    sleep(1);
+    cursor.moveup(game.getysize()+4);
+  end
 end
+
+
 
 
