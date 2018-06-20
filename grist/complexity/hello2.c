3a35956f5096eed1e81841d97505836282f79f8d
heusserm <matt.heusser@gmail.com>
2018-06-19 21:09:46 -0400

    Adjusted for ITKE

--- grist/complexity/hello2.c
+++ grist/complexity/hello2.c
@@ -6,10 +6,10 @@ int main()
     char strName[100];
     printf("\n\nWhat is your name? ");
     fgets(strName, 100, stdin);
-    if (strcmp(strName,"Matt")!=0) 
-      printf("\n\nHello, World!\n\n");
+    if (strcmp(strName,"Matt\n")!=0 && strcmp(strName,"Justin\n")!=0) 
+      printf("\n\nHello, %s\n\n",strName);
     else
-      printf("\n\nHello Matt!\n\n");
+      printf("\n\nHello ITKE Writer!\n\n\n");
     
     return 0;
 }
